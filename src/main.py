b8f73c80339dde29e457740284273b282e0e5fe5
maksud_BOSS <maksudurr173@gmail.com>
2026-06-12 21:53:12 +0600

    Add basic calculation functions in the project

--- src/main.py
+++ src/main.py
@@ -3,3 +3,9 @@ print('My name is Md. Maksudur Rahman')
 from datetime import date
 print("Today's date is ", date.today())
 
+
+from utils import addition,subtraction
+
+print(addition(10,10))
+print(subtraction(30,100))
+
